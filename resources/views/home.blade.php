29ab037f805b9681c0a6f64148454d5194720c6f
Matthew Pinder <33333622+B6019406@users.noreply.github.com>
2020-03-17 17:17:30 +0000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,43 +2,44 @@
 @extends('layouts.home')
 
 <style scoped>
-    
+
 </style>
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
 
-        </div>
     </div>
+  </div>
 
-    {{-- Add search bar here --}}
+  {{-- Add search bar here --}}
 
-    <div class="row justify-content-center">
-        <table class="table table-striped">
-            <thead class="thead-dark">
-              <tr>
-                <th scope="col">Website</th>
-                <th scope="col">Username</th>
-                <th scope="col">Password</th>
-              </tr>
-            </thead>
 
-            <tbody>
-              <tr>
-                <th><a href="http://amazon.co.uk">Amazon</th>
-                <td>Username</td>
-                <td>Password</td>
-              </tr>
+  <div class="row justify-content-center">
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Website</th>
+          <th scope="col">Username</th>
+          <th scope="col">Password</th>
+        </tr>
+      </thead>
 
-              <tr>
-                <th><a href="http://youtube.com">YouTube</th>
-                <td>Username</td>
-                <td>Password</td>
-              </tr>
-            </tbody>
-          </table>
-    </div>
+      <tbody>
+        <tr>
+          <th><a href="http://amazon.co.uk">Amazon</th>
+          <td>Username</td>
+          <td>Password</td>
+        </tr>
+
+        <tr>
+          <th><a href="http://youtube.com">YouTube</th>
+          <td>Username</td>
+          <td>Password</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
 @endsection
